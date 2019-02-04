5ab4a6068626b8d9ab802ca51be7c5b9650b5b50
dmousmane <dmousmane@gmail.com>
2019-02-04 22:27:06 +0100

    ajout des intents pour la page connexion et inscription

--- app/src/main/java/com/mamadou/diallo/healthapp/controller/MainActivity.java
+++ app/src/main/java/com/mamadou/diallo/healthapp/controller/MainActivity.java
@@ -1,15 +1,41 @@
 package com.mamadou.diallo.healthapp.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mamadou.diallo.healthapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mLogInBtn;
+    private Button mSignUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogInBtn = (Button) findViewById(R.id.activity_main_login_btn);
+        mSignUpBtn = (Button) findViewById(R.id.activity_main_signup_btn);
+
+        mLogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
