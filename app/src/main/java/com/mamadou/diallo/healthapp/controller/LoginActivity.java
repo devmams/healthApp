748cc9b3a11ac2daefcc5f2224320fe60a46271a
dmousmane <dmousmane@gmail.com>
2019-02-05 18:43:18 +0100

    home page create

--- app/src/main/java/com/mamadou/diallo/healthapp/controller/LoginActivity.java
+++ app/src/main/java/com/mamadou/diallo/healthapp/controller/LoginActivity.java
@@ -1,15 +1,37 @@
 package com.mamadou.diallo.healthapp.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mamadou.diallo.healthapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button mLogInTrueBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLogInTrueBtn = (Button) findViewById(R.id.activity_login_true_btn);
+
+        mLogInTrueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
+
+
 }
