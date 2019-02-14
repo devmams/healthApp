bcf956f53564eec6e2a97265ca8fec202ac49f36
dmousmane <dmousmane@gmail.com>
2019-02-14 11:16:46 +0100

    maj date calendar

--- app/src/main/java/com/mamadou/diallo/healthapp/controller/MakeAnAppointmentActivity.java
+++ app/src/main/java/com/mamadou/diallo/healthapp/controller/MakeAnAppointmentActivity.java
@@ -3,6 +3,7 @@ package com.mamadou.diallo.healthapp.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.mamadou.diallo.healthapp.R;
 
@@ -25,6 +26,9 @@ public class MakeAnAppointmentActivity extends AppCompatActivity {
         simpleCalendarView.setMinDate(min);
 
         calendar.add(Calendar.MONTH, 2);
+
+        int today = calendar.getActualMaximum(Calendar.DAY_OF_MONTH) - calendar.get(Calendar.DAY_OF_MONTH);
+        calendar.add(Calendar.DAY_OF_MONTH, today);
         Long max = calendar.getTime().getTime();
         simpleCalendarView.setMaxDate(max);
     }
