2135f095becbd520e18a49db7c864faf0ce8ad7a
dmousmane <dmousmane@gmail.com>
2019-03-05 01:31:24 +0100

    maj itineraire

---
+++ app/src/main/java/com/mamadou/diallo/healthapp/controller/ItineraireActivity.java
@@ -0,0 +1,15 @@
+package com.mamadou.diallo.healthapp.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mamadou.diallo.healthapp.R;
+
+public class ItineraireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itineraire);
+    }
+}
