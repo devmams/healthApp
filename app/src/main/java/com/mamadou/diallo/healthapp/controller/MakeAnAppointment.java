d61f3d515ffd4fa3cfcdc4e5971857d9be5b0e2b
yassinetif <yassinmchaar@gmail.com>
2019-03-06 18:16:28 +0100

    Modification de MakeAnAppointment

--- app/src/main/java/com/mamadou/diallo/healthapp/controller/MakeAnAppointment.java
+++ app/src/main/java/com/mamadou/diallo/healthapp/controller/MakeAnAppointment.java
@@ -1,16 +1,37 @@
 package com.mamadou.diallo.healthapp.controller;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.mamadou.diallo.healthapp.R;
 
-public class MakeAnAppointment extends AppCompatActivity {
+public class MakeAnAppointment extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_an_appointment);
+        Spinner spinner_1 = (Spinner) findViewById(R.id.spinner1);
+        spinner_1.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.speciality_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_1.setAdapter(adapter);
+    }
 
     @Override
     protected void onRestart() {
@@ -39,5 +60,15 @@ public class MakeAnAppointment extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
